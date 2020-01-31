33bb4c25ce710ba629da987a1e5eb31c65590f10
greentealemonade <kaydeee329@gmail.com>
2020-01-31 18:43:07 +0900

    Revert "real time interaction & try for camera"
    This reverts commit 5363f054fbaa6bdadeef0263da7f4f762baf6ec8.

--- photos/views.py
+++ photos/views.py
@@ -84,7 +84,6 @@ def homepage(request):
 
 # for Infinite Scroll
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from django.utils.crypto import get_random_string
 
 def main(request):
     username  = request.COOKIES.get('username', '')
@@ -110,12 +109,10 @@ def main(request):
     except EmptyPage:
         pics = paginator.page(paginator.num_pages)
 
-    unique_id = get_random_string(length=10)
 
     ctx = {
             'form': form,
             'pics': pics,
-            'string': unique_id,
     }
 
     if username:
