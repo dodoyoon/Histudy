10f404f04bf62dc85b63ec8595b50b89dcc25206
sungminkim <ksm3169@gmail.com>
2020-02-04 16:01:16 +0900

    change migrations

--- photos/migrations/0011_auto_20200204_0542.py
+++ photos/migrations/0011_auto_20200204_0542.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('photos', '0010_userinfo_name'),
+        ('photos', '0009_userinfo'),
     ]
 
     operations = [
