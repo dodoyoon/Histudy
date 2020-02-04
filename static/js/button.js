a41ca346438726c9efec19b1a450a83eb58e4a56
sungminkim <ksm3169@gmail.com>
2020-02-04 12:34:40 +0900

    Apply git ignore file

--- static/js/button.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-    $("#actasbutton").bind("click",function(){
-        $("#dataform").submit();  // consider idOfYourForm `id` of your form which you are going to submit
-    });
-});
-
-document.getElementById("actasbutton").onclick = function(){
-    document.getElementById("dataform").submit();
-};
