82540cb9a28f90e8ca55e3adb98240b764a842f1
sungminkim <ksm3169@gmail.com>
2020-03-15 19:14:28 +0900

    Change announcement_content.html

--- photos/admin.py
+++ photos/admin.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Data, UserInfo
+from .models import Data, UserInfo, Member
 from import_export.admin import ImportExportModelAdmin
 
 admin.site.register(Data)
 admin.site.register(UserInfo)
+admin.site.register(Member)
