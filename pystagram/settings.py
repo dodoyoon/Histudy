e0ceffbf44b8252ba4058bf56354475efc7ec820
greentealemonade <kaydeee329@gmail.com>
2020-03-22 16:55:25 +0800

    add allowed host

--- pystagram/settings.py
+++ pystagram/settings.py
@@ -23,7 +23,7 @@ SECRET_KEY = '*y_wwvq8k8$b$ll*73q45bji!)*d4*j=yc7hct%#c$fghf4ef('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['34.84.151.146', '127.0.0.1', '203.245.29.98']
+ALLOWED_HOSTS = ['34.84.151.146', '127.0.0.1', '203.245.29.98', 'shong03.cafe24.com']
 
 
 # Application definition
