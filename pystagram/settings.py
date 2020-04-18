c6c6a236199956e7c80a7149d04546c26d242fc8
greentealemonade <kaydeee329@gmail.com>
2020-04-18 17:15:06 +0900

    changed db setting to utf8mb4

--- pystagram/settings.py
+++ pystagram/settings.py
@@ -86,7 +86,8 @@ DATABASES = {
         'HOST': 'localhost',
         'PORT': '3306',
         'OPTIONS': {
-                'init_command': 'SET sql_mode = "STRICT_TRANS_TABLES"'
+                'init_command': 'SET sql_mode = "STRICT_TRANS_TABLES"',
+                'charset' : 'utf8mb4',
         }
     }
 }
