4f32eb2f31d211388f2d321dd9de8b6bb8f06b0e
greentealemonade <kaydeee329@gmail.com>
2020-08-22 23:28:09 +0900

    year db

--- photos/admin.py
+++ photos/admin.py
@@ -2,10 +2,11 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Data, UserInfo, Member, Announcement
+from .models import Data, UserInfo, Member, Announcement, Year
 from import_export.admin import ImportExportModelAdmin
 
 admin.site.register(Data)
 admin.site.register(UserInfo)
 admin.site.register(Member)
 admin.site.register(Announcement)
+admin.site.register(Year)
