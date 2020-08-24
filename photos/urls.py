5d80cb60610686595424285314b2c91382e7b815
sungminkim <ksm3169@gmail.com>
2020-08-24 14:55:55 +0900

    image download and import user now have year and semester filtering

--- photos/urls.py
+++ photos/urls.py
@@ -11,5 +11,6 @@ urlpatterns = [
     url(r'^password/$', views.change_password, name='change_password'),
     path('add_member/', views.add_member, name='add_member'),
     path('popup/', views.popup, name='popup'),
-    path('img_download/', views.img_download, name='img_download'),
+    path('img_download/<int:pk>/', views.img_download, name='img_download'),
+    path('img_download_page/', views.img_download_page, name='img_download_page'),
 ]
