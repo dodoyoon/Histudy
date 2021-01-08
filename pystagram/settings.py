5d147af5033a49a3428b01ec12a012c8e41d94a8
root <root@histudy.c.hybrid-circle-282107.internal>
2021-01-08 13:24:28 +0000

    allow all hosts

--- pystagram/settings.py
+++ pystagram/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = get_secret("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'histudy.cafe24.com']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
